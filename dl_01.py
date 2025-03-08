816f3ca0fdf94a70450fc1566305e301ac14ffc0
parksungsu <parkss1324@naver.com>
2025-03-09 01:37:13 +0900

    파이토치 시작

---
+++ dl_01.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# 공개 데이터셋에서 학습 데이터를 내려받습니다.
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# 공개 데이터셋에서 테스트 데이터를 내려받습니다.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+batch_size = 64
+
+# 데이터로더를 생성합니다.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
