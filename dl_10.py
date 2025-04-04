037cc57872f23f8f62c22ba7177d86bb02117f70
parksungsu <parkss1324@naver.com>
2025-04-04 10:21:03 +0900

    모델 매개변수 최적화

--- dl_10.py
+++ dl_10.py
@@ -39,3 +39,63 @@ class NeuralNetwork(nn.Module):
         return logits
 
 model = NeuralNetwork()
+
+# 하이퍼 파라미터
+epochs = 5 # 데이터셋을 반복하는 횟수
+batch_size = 64 # 매개변수가 갱신되기 전 신경망을 통해 전파된 데이터 샘플의 수
+learning_rate = 1e-3 # 각 epochs/batch에서 모델의 매개변수를 조절하는 비율
+                     # 값이 작으면 학습 속도가 느려지고, 커지면 학습 중 예측할 수 없는 동작이 발생할 수 있음
+
+# 손실 함수 초기화
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    # 모델을 학습(train) 모드로 설정 - 배치 정규화(Batch Normalization) 및 드롭아웃(Dropout) 레이어들에 중요
+    # 이 예시에서는 없어도 되지만, 모범 사례를 위해 추가
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        # 예측(prediction)과 손실(loss) 계산
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # 역전파
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader, model, loss_fn):
+    # 모델을 평가(eval) 모드로 설정 - 배치 정규화(Batch Normalization) 및 드롭아웃(Dropout) 레이어들에 중요
+    # 이 예시에서는 없어도 되지만, 모범 사례를 위해 추가
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    # torch.no_grad()를 사용하여 테스트 시 변화도(gradient)를 계산하지 않도록 해야함
+    # 이는 requires_grad=True로 설정된 텐서들의 불필요한 변화도 연산 및 메모리 사용량 또한 줄여줌
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
